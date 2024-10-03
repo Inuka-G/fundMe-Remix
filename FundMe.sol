2f261d9b5da7b846ea071e590742963ef5196755
Inuka-G <156221307+Inuka-G@users.noreply.github.com>
2024-10-03 20:45:19 +0530

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -3,17 +3,42 @@ pragma solidity 0.8.26;
 import "./PriceConverter.sol";
 
 contract FundMe {
-    using  PriceConverter for uint256;
+    using PriceConverter for uint256;
     uint256 public constant MININUM_USD_VALUE = 50 * 1e18;
-    address[] public  funders;
-    mapping (address=>uint256) addressToAmount;
+    address[] public funders;
+    mapping(address => uint256) addressToAmount;
+
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MININUM_USD_VALUE, "Didnt sent enough ETH");
+        require(
+            msg.value.getConversionRate() >= MININUM_USD_VALUE,
+            "Didnt sent enough ETH"
+        );
         funders.push(msg.sender);
-        addressToAmount[msg.sender]=msg.value;
+        addressToAmount[msg.sender] = msg.value;
     }
 
-   
+    function widthDraw() public {
+        for (int i = 0; i < funders.length(); i++) {
+            address funderAddress = funders[i];
+            addressToAmount[funderAddress] = 0;
+        }
+        funders = new address[](0);
 
-    
+        // withdraw the balance to the owner
+        // -----------------------------------------
+
+        // msg.sender is the address that call the widthDraw function
+        // address(this).balance is the balance(funds) of the contract
+        // 3 ways can transfer native currency to a address
+        // // transfer
+        // payable(msg.sender.transfer(address(this).balance));
+        // // send
+        // bool isSuccess = payable(msg.sender.send(address(this).balance));
+        // require(isSuccess, "Failed to send the funds");
+        // call
+        (bool isSucessCall /**return from calling func */, ) = payable(
+            msg.sender.call{value: address(this).balance}("")
+        );
+        require(isSucessCall, "Failed to send the funds");
+    }
 }
