6011f31b894f4707c26012ced4bcb71328f722af
Inuka-G <156221307+Inuka-G@users.noreply.github.com>
2024-10-04 10:05:15 +0530

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 import "./PriceConverter.sol";
+error NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
     uint256 public constant MININUM_USD_VALUE = 50 * 1e18;
     address[] public funders;
     mapping(address => uint256) addressToAmount;
+    adress public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
 
     function fund() public payable {
         require(
@@ -17,7 +23,7 @@ contract FundMe {
         addressToAmount[msg.sender] = msg.value;
     }
 
-    function widthDraw() public {
+    function widthDraw() public onlyOwner {
         for (int i = 0; i < funders.length(); i++) {
             address funderAddress = funders[i];
             addressToAmount[funderAddress] = 0;
@@ -40,5 +46,22 @@ contract FundMe {
             msg.sender.call{value: address(this).balance}("")
         );
         require(isSucessCall, "Failed to send the funds");
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "You are not the owner");
+
+        // for more gas optimization use this
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+
+// route to fund() function
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
